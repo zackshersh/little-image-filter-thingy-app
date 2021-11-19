d267eadf5f899e56b5744ff5ec1ae960655ff567
Zack Hersh <zackshersh@gmail.com>
2021-11-19 16:24:19 -0500

    filter.name deploy issue troubleshooting

--- src/components/FilterOption.js
+++ src/components/FilterOption.js
@@ -2,6 +2,8 @@ import React from "react";
 
 
 const FilterOption = ({handler,title,filter,defaultOptions}) => {
+    console.log(filter)
+    console.log(filter.name)
     return (
         <p data-filter={filter.name} data-options={defaultOptions} className="Filter-Option" onMouseDown={handler}>{title}</p>
     )
